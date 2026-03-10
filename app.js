d872d593ab88b106e32a1490004a39b4b630b391
Nitish kumar Singh <kumarsinghnitish89@gmail.com>
2026-03-11 00:14:10 +0530

    Fixed ejs-mate layout integration and CSS setup

--- app.js
+++ app.js
@@ -20,12 +20,13 @@ main()
     await mongoose.connect(MONGO_URL);
  }
 
+ app.engine("ejs", ejsMate);
  app.set("view engine", "ejs");
+ 
  app.set("views",  path.join(__dirname, "views"));
- //console.log(path.join(__dirname, "views"));
+ console.log(path.join(__dirname, "views"));
  app.use(express.urlencoded({extended: true}));
  app.use(methodOverride("_method"));
- app.engine("ejs", ejsMate);
  app.use(express.static(path.join(__dirname, "public")));
 
 
