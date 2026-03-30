b0a316e7b092d3a0a4b62019fef4146b0aba83f1
Nitish kumar Singh <kumarsinghnitish89@gmail.com>
2026-03-31 01:13:07 +0530

    Completed every edge case of deletion regarding reviews

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review");
 
 const listingSchema = new Schema({
     title: {
@@ -35,6 +36,14 @@ const listingSchema = new Schema({
         ref: "Review"
     }
 ]
+});
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+    if(listing){
+        await Review.deleteMany({
+            _id: { $in: listing.reviews }
+        });
+    }
 });
 
 
